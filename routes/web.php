3fdae641a30174e56ad77b420cf4e835982c8f9b
Carlos Telles <carlostelles.noia@gmail.com>
2024-03-17 23:58:08 -0300

    feat: configurado docs

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('docs');
 });
 
 Route::get('/file/{file}', function ($file) {
